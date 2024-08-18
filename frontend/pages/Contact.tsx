ab2e4e16c0af45ed9be6218f3502538a01a7202b
Sebas <disalvatore.sebastian@gmail.com>
2024-08-18 17:46:10 -0300

    adding resume page

--- frontend/pages/Contact.tsx
+++ frontend/pages/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
       className="py-24 flex flex-col  items-center w-full h-[80rem]  bg-black/50 text-white"
       id="contact"
     >
-      <h1 className="text-5xl text-center font-bold mb-10">Resume</h1>
+      <h1 className="text-5xl text-center font-bold mb-10">Contact</h1>
       <div className="container mx-auto p-6 w-full h-[50rem] bg-gray-100/90"></div>
     </section>
   );
