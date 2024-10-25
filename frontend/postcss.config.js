789cf071fac8bc6ea6996c7cf627d6440d4fdfb0
Sebastian Di Salvatore <115433902+Sebads7@users.noreply.github.com>
2024-10-25 10:09:14 -0300

    Delete frontend directory

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: [
-    "postcss-import", // Optional: Enables @import for CSS files
-    "tailwindcss", // Enables Tailwind CSS
-    "autoprefixer", // Adds vendor prefixes for better browser support
-  ],
-};
