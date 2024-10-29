b669af7cdff2256394dba2487a397cc60e30fd17
Sebastian Di Salvatore <115433902+Sebads7@users.noreply.github.com>
2024-10-29 16:36:47 -0300

    Delete frontend directory

--- frontend/utils/generateRandomColor.ts
+++
@@ -1,9 +0,0 @@
-// Define a list of colors, including predefined ones
-const predefinedColors = ["#FFFFFF", "#F8F6F4", "#FAFAF6", "#FFFFFF"];
-
-// Function to generate a random color from predefined colors
-export const generateRandomColor = (): string => {
-  // Get a random index from the predefined colors array
-  const randomIndex = Math.floor(Math.random() * predefinedColors.length);
-  return predefinedColors[randomIndex];
-};
